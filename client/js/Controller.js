bc323d496e11552dcb44bd343f582c17da06ec6b
dominikcsiszer <dominikcsiszer@icloud.com>
2023-05-31 13:16:38 +0200

    Végső

--- client/js/Controller.js
+++ client/js/Controller.js
@@ -1,15 +1,35 @@
 import ApiModel from "./ApiModel.js"
+import View from "./View.js"
 
 class Controller {
     
     constructor() {
-        console.log("asd");
         const model = new ApiModel()
-        model.fetchData('http://localhost:5000/', this.data)
+        model.fetchData('http://localhost:5000/kategoria', this.kategoria)
+
+        const kategira = document.querySelector(".form-select")
+        model.fetchData(`http://localhost:5000/tesztek/kategoria/1`, this.data)
+
+        kategira.addEventListener('change', () => {
+            model.fetchData(`http://localhost:5000/tesztek/kategoria/${kategira.value}`, this.data)
+        })
     }
     
     data(data) {
-        console.log(data);
+        const test = document.querySelector(".tesztek")
+        test.innerHTML = ""
+        data.forEach(element => {
+            new View(element)  
+        })
+    }
+
+    kategoria(data) {
+        const kategiraSelect = document.querySelector(".form-select")
+        data.forEach(element => {
+            let html = `<option value="${element.id}">${element.kategorianev}</option>`
+            kategiraSelect.insertAdjacentHTML('beforeend', html)
+        })
+
     }
 }
 
--- client/js/View.js
+++ client/js/View.js
@@ -1,15 +1,28 @@
 class View {
 
     #adat
-    #parent
-    constructor(adat, parent) {
+    constructor(adat) {
         this.#adat = adat
-        this.#parent = $(parent)
         this.html()
     }
 
     html() {
-        console.log(this.#adat);
+        const test = document.querySelector(".tesztek")
+        let html = `<div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">${this.#adat.kerdes}</h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="row row-cols-1 row-cols-md-2">
+                            <div class="col kerdes" id="v1">${this.#adat.v1}</div>
+                            <div class="col kerdes" id="v2">${this.#adat.v3}</div>
+                            <div class="col kerdes" id="v3">${this.#adat.v4}</div>
+                            <div class="col kerdes" id="v4">${this.#adat.v2}</div>
+                        </div>
+                    </div>
+                </div>`
+        test.insertAdjacentHTML('beforeend', html)
+
     }
 }
 
